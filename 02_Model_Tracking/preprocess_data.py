fdedd7c0ad92014d297e4515e8762ab3a5d7343f
Dheeraj Karra <dheerajkarra@gmail.com>
2022-05-30 10:17:36 +0530

    Added week2

---
+++ 02_Model_Tracking/preprocess_data.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 29 17:05:23 2022
+
+@author: Dheeraj
+"""
+
+# Notes
+# 1. mlflow, version 1.26.1
+# 2.
+# python preprocess_data.py --raw_data_path C:\\Users\\Satya\\Desktop\\Projects\\MLOps\\Data --dest_path C:\\Users\\Satya\\Desktop\\Projects\\MLOps\\MLOps_Zoomcamp\\02_Model_Tracking\\output
+
+# 4 files were saved
+
+# 3. 17 parameters
+# python train.py --data_path C:\\Users\\Satya\\Desktop\\Projects\\MLOps\\MLOps_Zoomcamp\\02_Model_Tracking\\output
+
+# 4.
+# mlflow ui --backend-store-uri sqlite:///mlflow.db --serve-artifacts
+
+# python hpo.py --data_path C:\\Users\\Satya\\Desktop\\Projects\\MLOps\\MLOps_Zoomcamp\\02_Model_Tracking\\output --max_evals 50
+
+# 5. 6.628 (step=0) RMSE
+
+# 6. test RMSE of best model - 6.548
+
+# python register_model.py --data_path C:\\Users\\Satya\\Desktop\\Projects\\MLOps\\MLOps_Zoomcamp\\02_Model_Tracking\\output 
+
+
+
+
+import argparse
+import os
+import pickle
+
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+
+
+def dump_pickle(obj, filename):
+    with open(filename, "wb") as f_out:
+        return pickle.dump(obj, f_out)
+
+
+def read_dataframe(filename: str):
+    df = pd.read_parquet(filename, engine='pyarrow')
+
+    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+
+    return df
+
+
+def preprocess(df: pd.DataFrame, dv: DictVectorizer, fit_dv: bool = False):
+    df['PU_DO'] = df['PULocationID'] + '_' + df['DOLocationID']
+    categorical = ['PU_DO']
+    numerical = ['trip_distance']
+    dicts = df[categorical + numerical].to_dict(orient='records')
+    if fit_dv:
+        X = dv.fit_transform(dicts)
+    else:
+        X = dv.transform(dicts)
+    return X, dv
+
+
+def run(raw_data_path: str, dest_path: str, dataset: str = "green"):
+    # load parquet files
+    df_train = read_dataframe(
+        os.path.join(raw_data_path, f"{dataset}_tripdata_2021-01.parquet")
+    )
+    df_valid = read_dataframe(
+        os.path.join(raw_data_path, f"{dataset}_tripdata_2021-02.parquet")
+    )
+    df_test = read_dataframe(
+        os.path.join(raw_data_path, f"{dataset}_tripdata_2021-03.parquet")
+    )
+
+    # extract the target
+    target = 'duration'
+    y_train = df_train[target].values
+    y_valid = df_valid[target].values
+    y_test = df_test[target].values
+
+    # fit the dictvectorizer and preprocess data
+    dv = DictVectorizer()
+    X_train, dv = preprocess(df_train, dv, fit_dv=True)
+    X_valid, _ = preprocess(df_valid, dv, fit_dv=False)
+    X_test, _ = preprocess(df_test, dv, fit_dv=False)
+
+    # create dest_path folder unless it already exists
+    os.makedirs(dest_path, exist_ok=True)
+
+    # save dictvectorizer and datasets
+    dump_pickle(dv, os.path.join(dest_path, "dv.pkl"))
+    dump_pickle((X_train, y_train), os.path.join(dest_path, "train.pkl"))
+    dump_pickle((X_valid, y_valid), os.path.join(dest_path, "valid.pkl"))
+    dump_pickle((X_test, y_test), os.path.join(dest_path, "test.pkl"))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--raw_data_path",
+        help="the location where the raw NYC taxi trip data was saved"
+    )
+    parser.add_argument(
+        "--dest_path",
+        help="the location where the resulting files will be saved."
+    )
+    args = parser.parse_args()
+
+    run(args.raw_data_path, args.dest_path)
